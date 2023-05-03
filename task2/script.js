18d3c8f7288be5d80e8fe16ad9d74ddd68558f9a
shahana jabin <jabin@bititude.com>
2023-05-03 13:57:38 +0530

    task 3 created

--- task2/script.js
+++ task2/script.js
@@ -1,5 +1,14 @@
+let country = 'India';
+let continent = 'Asia';
+let population = 1418237140;
+console.log(country);
+console.log(continent);
+console.log(population);
+
 let IndiaisIsland = false;
 console.log(IndiaisIsland);
+
+//task2
 
 let language;
 console.log(language);
