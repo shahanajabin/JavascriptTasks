72a69170d3de6cec8479d132a7c944d81c893624
shahana jabin <jabin@bititude.com>
2023-05-03 12:04:14 +0530

    task 2 created

---
+++ task2/script.js
@@ -0,0 +1,11 @@
+let IndiaisIsland = false;
+console.log(IndiaisIsland);
+
+let language;
+console.log(language);
+
+console.log(typeof IndiaisIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
